2cfaabdf133fefb01017e7d3fbdb5c1de6feebab
KunalP99 <kunal_patel200@hotmail.co.uk>
2022-08-01 17:26:04 +0100

    Designed the main style of the page

---
+++ app.js
@@ -0,0 +1,23 @@
+let myLibrary = [{
+    title: "Game of Thrones",
+    author: "George R.R. Martin",
+    pages: 694,
+    hasRead: false
+},
+{
+    title: "Harry Potter and the Chamber of Secrets",
+    author: "J.K. Rowling",
+    pages: 223,
+    hasRead: true
+}];
+
+const cardsContainer = document.querySelector('.cards-container');
+console.log(cardsContainer);
+
+function Book() {
+    
+}
+
+function addBookToLibrary() {
+
+}
