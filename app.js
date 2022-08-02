21f7b76750d85c1d50f019d4d11aba842c34e9ea
KunalP99 <kunal_patel200@hotmail.co.uk>
2022-08-02 22:51:41 +0100

    Added error check for empty input fields

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
-let myLibrary = [{
-    title: "Game of Thrones",
-    author: "George R.R. Martin",
-    pages: 694,
-    hasRead: false
-},
-{
-    title: "Harry Potter and the Chamber of Secrets",
-    author: "J.K. Rowling",
-    pages: 223,
-    hasRead: true
-}];
+let myLibrary = [];
 
 const cardsContainer = document.querySelector('.cards-container');
 const modalOverlay = document.querySelector('.modal-overlay');
+
+// Buttons
 const addBtn = document.getElementById('plus-btn');
+const addBookBtn = document.getElementById('add-book-btn');
+
+// Form variables
+const form = document.getElementById('book-form');
+let titleInput;
+let authorInput;
+let pagesInput;
+let hasReadInput;
 
 // Displays the modal overlay 
 addBtn.addEventListener('click', () => {
@@ -26,10 +25,32 @@ window.onclick = (e) => {
     }
 };
 
+addBookBtn.addEventListener('click', () => {
+    let titleValue = document.getElementById('title').value;
+    let authorValue = document.getElementById('author').value;
+    let pagesValue = document.getElementById('pages').value;
+
+    if (titleValue.length == 0 || authorValue.length == 0 || pagesValue.length == 0) {
+        console.log('error');
+        return 'error';
+    } else {
+        titleInput = titleValue;
+        authorInput = authorValue;
+        pagesInput = pagesValue;
+        hasReadInput = document.getElementById('hasRead').value;
+    }
+        console.log(titleInput, authorInput, pagesInput, hasReadInput);
+});
+
+const displayBook = () => {
+
+}
+
 function Book() {
     
 }
 
-function addBookToLibrary() {
-
-}
+const addBookToLibrary = () => {
+    // Form Data
+    return Array.from(document.querySelectorAll('#book-form input')).reduce((acc, input) => ({ ...acc, [input.id] : input.value }), {});
+};
